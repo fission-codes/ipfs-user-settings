d21aea518a64d7ed83b6abf6ebe8c28ee1ef9f48
Daniel Holmgren <dtholmgren@gmail.com>
2019-08-19 19:53:05 -0500

    validate preferences on save & load (#14)

--- src/ipfs/preferences.js
+++ src/ipfs/preferences.js
@@ -1,5 +1,21 @@
 import getIpfs from "../ipfs/getIpfs";
 
+export const DefaultCid = "QmUMQ5Zxu94gwGq96hGEBc2hzoMkywUctbySw7YY6g8ktw";
+
+export const validPreferences = preferences => {
+  return (
+    preferences.codeStyle &&
+    preferences.primaryColor &&
+    preferences.primaryColor.length === 7 &&
+    preferences.secondaryColor &&
+    preferences.secondaryColor.length === 7 &&
+    preferences.theme &&
+    (preferences.theme === "light" || preferences.theme === "dark") &&
+    preferences.username &&
+    preferences.codeStyle
+  );
+};
+
 export const loadPreferences = async cid => {
   const ipfs = await getIpfs();
   let resp, preferences;
@@ -13,13 +29,16 @@ export const loadPreferences = async cid => {
   } catch (err) {
     throw new Error("CID does not contain preferences");
   }
-  if (!preferences.codeStyle) {
-    throw new Error("CID does not contain preferences");
+  if (!validPreferences(preferences)) {
+    throw new Error("Preferences are outdated. Try saving a new version.");
   }
   return preferences;
 };
 
 export const savePreferences = async preferences => {
+  if (!validPreferences(preferences)) {
+    throw new Error("Preferences are not valid. Try saving a new version.");
+  }
   const ipfs = await getIpfs();
   const toAdd = Buffer.from(JSON.stringify(preferences));
   let resp, cid;
