a4eb840307621bd1eeafef515c6db90b4d2efb41
Pat Dryburgh <hello@patdryburgh.com>
2019-10-23 15:36:32 -0700

    display preferences object in preview panel

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -77,6 +77,12 @@ class Preview extends React.Component {
             >
               {codeSample}
             </SyntaxHighlighter>
+            <SyntaxHighlighter
+              language="json"
+              style={codeStyles[preferences.codeStyle]}
+            >
+              {JSON.stringify(preferences, null, 2)}
+            </SyntaxHighlighter>
           </Container>
         </MuiThemeProvider>
       </Paper>
